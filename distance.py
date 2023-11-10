6900ac3ea1c176b348d30316d876e22bd9e0bac6
Bhanuprakashreddy143 <140944008+Bhanuprakashreddy143@users.noreply.github.com>
2023-11-10 11:09:39 +0530

    Distance-first program
    distance between

---
+++ distance.py
@@ -0,0 +1,11 @@
+>>> x1=float(input('Enter  x1-corrdinate'))
+Enter  x1-corrdinate-5
+>>> y1=float(input('Enter  y1-corrdinate'))
+Enter  y1-corrdinate3
+>>> x2=float(input('Enter  x2-corrdinate'))
+Enter  x2-corrdinate-1
+>>> y2=float(input('Enter  y2-corrdinate'))
+Enter  y2-corrdinate7
+>>> distance=(((x1-x2)**2)+(y1-y2)**2)**1/2
+>>> print('the distance between two points :',distance)
+the distance between two points : 16.0
